e5faa3a45d8c03c61be657bf54ca0fb7088e02ea
psj <993097514@qq.com>
2022-07-09 13:08:12 +0800

    1308

---
+++ src/main/java/com/example/testpanshijun/TestpanshijunApplication.java
@@ -0,0 +1,13 @@
+package com.example.testpanshijun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestpanshijunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestpanshijunApplication.class, args);
+    }
+
+}
